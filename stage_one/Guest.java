b3c42c4024de8b117b71c40a750814b373bc4fd5
David Brooks <davidbrooks92@hotmail.com>
2021-11-30 14:30:12 +0000

    end of test

--- stage_one/Guest.java
+++ stage_one/Guest.java
@@ -1,5 +1,20 @@
 package stage_one;
 
 public class Guest {
+    String surname = "";
+    int bill;
 
+    public Guest() {
+        bill = 0;
+    }
+
+    public void guestName() {
+        String printName = surname;
+        System.out.println(printName);
+    }
+
+    public static void Guest(String[] args) {
+        Guest myObj = new Guest();
+        System.out.println(myObj.bill);
+    }
 }
