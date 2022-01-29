c35249ced523d814c50489606deb973a9a0abc0b
David Brooks <davidbrooks92@hotmail.com>
2022-01-29 12:01:53 +0000

    Test

--- stage_one/Guest.java
+++ stage_one/Guest.java
@@ -17,16 +17,18 @@ public class Guest {
         String myObj = surname.nextLine();
         System.out.println("Surname is: " + myObj);
         System.out.println("Would you like to book a night? Yes or No");
-        Scanner myobja = new Scanner(System.in);
-        if ("Yes".equals(myobja.nextLine())) {
-            addToBill();
+        try (Scanner myobja = new Scanner(System.in)) {
+            if ("Yes".equals(myobja.nextLine())) {
+                addToBill();
+            }
         }
     }
 
     public void addToBill() {
         System.out.println("How many nights would you like? (28 days maximum)");
-        Scanner daysT = new Scanner(System.in);
-        bill = bill * daysT.nextInt();
+        try (Scanner daysT = new Scanner(System.in)) {
+            bill = bill * daysT.nextInt();
+        }
     }
 
     public void guestName() {
@@ -35,11 +37,4 @@ public class Guest {
     }
     // This -v- needs to be moved to HotelTest.java, try to mve all I/O into the
     // test harness which will be in HotelTest.java
-
-    public static void main(String[] args) {
-
-        Guest myObj = new Guest();
-        System.out.println("Your total bill is: " + myObj.bill);
-        System.out.println("For " + myObj.days + " days");
-    }
 }
--- stage_one/RoomOne.java
+++ stage_one/RoomOne.java
@@ -1,5 +1,12 @@
 package stage_one;
 
 public class RoomOne {
-    Guest bookings[];
+    Guest[] bookings;
+
+    public static void main(String[] args) {
+
+        var myObj = new Guest();
+        System.out.println("Your total bill is: " + myObj.bill);
+        System.out.println("For " + myObj.days + " days");
+    }
 }
